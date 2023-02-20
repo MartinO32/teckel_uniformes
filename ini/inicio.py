fb0d61a44ed666cc52c0977454946d4227caa087
Martin Oller <martinoller@hotmail.com>
2023-02-19 21:01:55 -0300

    Modulo de contactos

--- ini/inicio.py
+++ ini/inicio.py
@@ -7,8 +7,9 @@ from PyQt5.uic import loadUi
 from fecha import FechaDeHoy
 #from gestion_pedidos import Gestion_pedidos
 #from deposito import Deposito
-#from contactos import Contactos
-
+from contactos import Contactos
+sys.path.append("db")
+from bbdd import Bbdd
 
 class teckel(QMainWindow):
 	def __init__(self):
@@ -16,11 +17,14 @@ class teckel(QMainWindow):
 		loadUi('./assets/gui/inicio.ui', self)
 		#self.btn_gestion.clicked.connect(self.gestionar)
 		#self.btn_deposito.clicked.connect(self.abrir_deposito)
-		#self.btn_contactos.clicked.connect(self.abrir_contactos)
+		self.btn_contactos.clicked.connect(self.abrir_contactos)
 		hoy=FechaDeHoy().fecha_actual()
 		self.fecha_hoy.setText(f'Hoy es {hoy[0]} {hoy[1]}\nde {hoy[2]}\n de {hoy[3]}')
+		Bbdd().create()
 		
-#Funciones de Botones
+		
+#Funciones
+	
 	#Abrir ventana Gestion
 	""" def gestionar(self):
 		ventana=Gestion_pedidos(self)
@@ -30,11 +34,12 @@ class teckel(QMainWindow):
 	def abrir_deposito(self):
 		ventana=Deposito(self)
 		ventana.show() 
+		"""
 
 	#Abrir ventana contactos
 	def abrir_contactos(self):
 		ventana=Contactos(self)
-		ventana.show()  """
+		ventana.show()  
 
 if __name__=='__main__':
 	app = QApplication(sys.argv)
